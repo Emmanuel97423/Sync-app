68b4cac04cdc62137f4b17bdc13514361c384da5
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-09-02 17:51:29 +0400

    Feature-test(EBP-SDK): change sdk command

--- utils/cronJobSdk.js
+++ utils/cronJobSdk.js
@@ -5,7 +5,7 @@ const job = new CronJob(
     '*/2 * * * *',
     () => {
         console.log('You will see this message every 2 minute');
-        exec('"C:/Program Files/EBP/PointOfSale21.1FRFR30/EBP.Invoicing.Application.exe"/BatchFile="D:/Documents/Exo-trap/EBP/Synchronisation/Sync-app/assets/import/batch/ebp-export.txt"', (error, stdout, stderr) => {
+        exec('"C:/Program Files/EBP/PointOfSale21.1FRFR30/EBP.Invoicing.Application.exe"\Database="D:\Documents\Exo Trap.ebp";SDKDEV;Dropknee97460?/BatchFile="D:/Documents/Exo-trap/EBP/Synchronisation/Sync-app/assets/import/batch/ebp-export.txt"', (error, stdout, stderr) => {
             if (error) console.log({ error });
             console.log({ stdout });
             console.error({ stderr });
