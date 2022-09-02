6b0897de37f9fb3020b2ca6eb0b544651b46ae15
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-09-02 13:52:30 +0400

    Feature-test(EBP-SDK): test classic command export articles

--- utils/cronJobSdk.js
+++ utils/cronJobSdk.js
@@ -5,7 +5,7 @@ const job = new CronJob(
     ' * * * * *',
     () => {
         console.log('You will see this message every minute');
-        exec('"C:\Program Files\EBP\PointOfSale21.1FRFR30\EBP.Invoicing.Application.exe" /BatchFile="D:\Documents\Exo-trap\EBP\Synchronisation\Sync-app\assets\import\batch\ebp-export.txt"', (error, stdout, stderr) => {
+        exec('"C:\Program Files\EBP\PointOfSale21.1FRFR30\EBP.Invoicing.Application.exe" /Gui=False /Database="D:\Documents\Exo Trap.ebp";SDKDEV;Dropknee97460? /Export="D:\Documents\Exo-trap\EBP\Synchronisation\Sync-app\assets\import\production\articles.txt";Items;Export-articles"', (error, stdout, stderr) => {
             if (error) console.log({ error });
             console.log({ stdout });
             console.error({ stderr });
