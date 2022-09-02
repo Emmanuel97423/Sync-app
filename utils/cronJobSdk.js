72ad57a61e970b39c9681f37441a0fb58292f83a
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-09-02 15:03:44 +0400

    Feature-test(EBP-SDK): change cron at all 5 min

--- utils/cronJobSdk.js
+++ utils/cronJobSdk.js
@@ -2,10 +2,10 @@ const { exec } = require('child_process');
 
 const CronJob = require('cron').CronJob;
 const job = new CronJob(
-    ' 05 * * * *',
+    '15 * * * *',
     () => {
         console.log('You will see this message every minute');
-        exec('"C:/Program Files/EBP/PointOfSale21.1FRFR30/EBP.Invoicing.Application.exe" /Gui=False /Database="D:/Documents/Exo Trap.ebp";SDKDEV;Dropknee97460? /Export="D:/Documents/Exo-trap/EBP/Synchronisation/Sync-app/assets/import/production/articles.txt";Items;Export-articles"', (error, stdout, stderr) => {
+        exec('"C:/Program Files/EBP/PointOfSale21.1FRFR30/EBP.Invoicing.Application.exe" /BatchFile="D:/Documents/Exo-trap/EBP/Synchronisation/Sync-app/assets/import/batch/ebp-export.txt"', (error, stdout, stderr) => {
             if (error) console.log({ error });
             console.log({ stdout });
             console.error({ stderr });
