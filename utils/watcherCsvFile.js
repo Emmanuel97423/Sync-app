70226a442551b002bcf61be19b84ac8d3626bd67
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-09-06 10:28:54 +0400

    Bugfix(sdk-batch): Test

--- utils/watcherCsvFile.js
+++ utils/watcherCsvFile.js
@@ -1,14 +1,14 @@
 const chokidar = require('chokidar');
 const axios = require('axios');
-import ConvertProduct from './classes/convertClasse';
+// import ConvertProduct from './classes/convertClasse';
 
 
 const filesPath = "./assets/import/production/"
-const articlesGammesFile = "./assets/import/production/articles-gamme.txt";
-const articlesFile = "./assets/import/production/articles.txt";
-const gammesFile = "./assets/import/production/gamme.txt";
-const familleFile = "./assets/import/production/familles.txt";
-const sousFamilleFile = "./assets/import/production/sous-famille.txt";
+// const articlesGammesFile = "./assets/import/production/articles-gamme.txt";
+// const articlesFile = "./assets/import/production/articles.txt";
+// const gammesFile = "./assets/import/production/gamme.txt";
+// const familleFile = "./assets/import/production/familles.txt";
+// const sousFamilleFile = "./assets/import/production/sous-famille.txt";
 
 
 // const productsCsv = './assets/import/production/articles.txt';
@@ -39,92 +39,9 @@ const watcherCsvFile = async () => {
         ])
 
     })
-    return
+ 
 
-    watcher.on("change", async (event, path) => {
-        axios.post(process.env.BASE_URL + "/api/import/product-gammes").then(() => {
-            axios.get(process.env.BASE_URL + "/api/products").then(() => {
-                console.log('finish')
-            })
-        })
-
-    })
-
-    watcher.on("change", (event, path) => {
-        console.log(event, path);
-        const convertProduct = new ConvertProduct(articlesFile, articlesGammesFile, gammesFile);
-        convertProduct.getProductCsvToJson;
-
-
-        const convertProductGamme = require('./convertProductGamme')
-
-        if (event === "change") {
-            convertProductGamme;
-            return
-            const convertProduct = new ConvertProduct(articlesFile, articlesGammesFile, gammesFile);
-            convertProduct.getProductCsvToJson;
-
-
-        }
-
-    }).then(() => {
-        console.log("finish")
-    });
-
-
-
-    // chokidar.watch(filePath, {
-    //     ignored: /(^|[\/\\])\../, // ignore dotfiles
-    //     persistent: true,
-    //     ignoreInitial: true,
-    //     awaitWriteFinish: {
-    //         stabilityThreshold: 2000,
-    //         pollInterval: 100
-    //     },
-    //     alwaysStat: true,
-    // }, (event, path) => {
-    //     console.log('path:', path)
-    //     console.log('event:', event)
-
-    // });
 }
-// const watcherCsvFileV1 = () => {
 
-//     console.log(filePath)
-//     // Initialize watcher.
-//     const watcher = chokidar.watch(filePath, {
-//         ignored: /(^|[\/\\])\../, // ignore dotfiles
-//         persistent: true,
-//         ignoreInitial: true,
-//         awaitWriteFinish: {
-//             stabilityThreshold: 2000,
-//             pollInterval: 100
-//         },
-//         alwaysStat: true,
-//     });
-
-//     watcher.on('change', (path) => {
-//         console.log(`File ${path} has been changed`)
-//         setTimeout(() => {
-//             try {
-//                 //Import request
-
-//                 http.get('http://localhost:3000/api/import', (res) => {
-//                     console.log('statusCode:', res.statusCode);
-//                     console.log('headers:', res.headers);
-//                     res.on('data', (d) => {
-//                         process.stdout.write(d);
-//                     });
-
-//                 }).on('error', (e) => {
-//                     console.error(e);
-//                 });
-//             } catch (error) {
-//                 console.log(error)
-
-//             }
-//         }, 20000)
-//     }).on('error', error => log(`Watcher error: ${error}`))
-// };
 
 module.exports = watcherCsvFile()
