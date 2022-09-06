0a15eb87c1012c0b94deedb2bc5d34509c77c64f
Emmanuel Narcisse <epokweb@hotmail.fr>
2022-09-06 11:05:28 +0400

    Bugfix(sdk-batch): Test

--- utils/cronJobSdk.js
+++ utils/cronJobSdk.js
@@ -5,7 +5,7 @@ const job = new CronJob(
     '*/1 * * * *',
     () => {
         console.log('You will see this message every 5 minute');
-        exec('"C:\Program Files\EBP\PointOfSale21.1FRFR30\EBP.Invoicing.Application.exe" /Gui=False /Database="D:\Documents\Exo Trap.ebp";SDKDEV;Dropknee97460? /Export="D:\Documents\Exo-trap\EBP\Synchronisation\Sync-app\assets\import\production\articles.txt";Items;Export-articles"', (error, stdout, stderr) => {
+        exec('"C:/Program Files/EBP/PointOfSale21.1FRFR30/EBP.Invoicing.Application.exe" /Gui=False /Database="D:/Documents/Exo Trap.ebp";SDKDEV;Dropknee97460? /Export="D:/Documents/Exo-trap/EBP/Synchronisation/Sync-app/assets/import/production/articles.txt";Items;Export-articles"', (error, stdout, stderr) => {
             if (error) console.log({ error });
             console.log({ stdout });
             console.error({ stderr });
